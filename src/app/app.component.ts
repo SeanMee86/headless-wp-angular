7b84f3a316101d3d54e11f52de9ecd1577bda70d
SeanMee86 <sean.mee86@yahoo.com>
2018-11-02 16:55:46 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'headless-angular';
+export class AppComponent implements OnInit {
+  projects = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost/wp-json/wp/v2/pages').subscribe((data) => {
+      for (const key in data) {
+        if (data.hasOwnProperty(key)) {
+          this.projects.push((data[key]));
+        }
+      }
+      console.log(this.projects);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
